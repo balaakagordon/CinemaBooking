5edab50116124fb48c4815505d135328c9cf368d
Gordon Balaaka <40589388+balaakagordon@users.noreply.github.com>
2019-07-16 12:18:48 +0300

    Revert "add db, routes, user controller and user model"

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,89 +1,70 @@
-var Users = require('../../controllers/UserController');
+const express = require('express');
+const uuid = require('uuid');
+const users = require('../../Users');
 
-module.exports = function(router) {
-    router.get('/', Users.getUsers);
-    router.post('/', Users.createUser);
-    router.get('/:id', Users.getUser)
-    router.put('/update/:id', Users.updateUser)
-    router.delete('/remove/:id', Users.removeUser)
-}
+const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.json(users);
+});
 
+router.get('/:id', (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id));
+    if(found) {
+        res.json(users.filter(user => user.id === parseInt(req.params.id)));
+    } else {
+        res.status(400).json({msg: `No user with the id of ${req.params.id}`});
+    }
+});
 
-// const express = require('express');
-// const uuid = require('uuid');
-// const users = require('../../Users');
-// const db = require('../../db');
-// const dbHelpers = require('../../services/dbService');
-// var noSqlDB = new dbHelpers.noSqlDB();
+router.post('/', (req, res) => {
+    let newUser = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: 'active',
+    }
 
-// const router = express.Router();
+    if( !newUser.name || !newUser.name ) {
+        return res.status(400).json({ msg: 'Please submit your name and email' })
+    }
+    users.push(newUser);
 
-// router.get('/', async (req, res) => {
-//     const collection = 'users';
-//     var response = await noSqlDB.getAllFields(collection);
-//     console.log('res =====> ', response);
-//     res.json(response);
-// });
+    res.status(201).json(users);
+})
 
-// router.get('/:id', (req, res) => {
-//     const found = users.some(user => user.id === parseInt(req.params.id));
-//     if(found) {
-//         res.json(users.filter(user => user.id === parseInt(req.params.id)));
-//     } else {
-//         res.status(400).json({msg: `No user with the id of ${req.params.id}`});
-//     }
-// });
+router.put('/:id', (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id));
 
-// router.post('/', (req, res) => {
-//     let newUser = {
-//         id: uuid.v4(),
-//         name: req.body.name,
-//         email: req.body.email,
-//         status: 'active',
-//     }
+    if (found) {
+        const updateUser = req.body;
+        users.forEach(user => {
+            if(user.id === parseInt(req.params.id)) {
+                user.name = updateUser.name ? req.body.name : user.name,
+                user.email = updateUser.email ? req.body.email : user.email
+                return res.status(200).json(
+                    {
+                        msg: 'User updated',
+                        data: user
+                    }
+                );
+            }
+        })
+    } else {
+        res.status(400).json({msg: `No user with the id of ${req.params.id}`});
+    }
+})
 
-//     if( !newUser.name || !newUser.name ) {
-//         return res.status(400).json({ msg: 'Please submit your name and email' })
-//     }
-//     users.push(newUser);
+router.delete('/:id', (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id));
+    if(found) {
+        res.json(
+            {
+                msg: 'User deleted',
+                users: users.filter(user => user.id !== parseInt(req.params.id))});
+    } else {
+        res.status(400).json({msg: `No user with the id of ${req.params.id}`});
+    }
+});
 
-//     res.status(201).json(users);
-// })
-
-// router.put('/:id', (req, res) => {
-//     const found = users.some(user => user.id === parseInt(req.params.id));
-
-//     if (found) {
-//         const updateUser = req.body;
-//         users.forEach(user => {
-//             if(user.id === parseInt(req.params.id)) {
-//                 user.name = updateUser.name ? req.body.name : user.name,
-//                 user.email = updateUser.email ? req.body.email : user.email
-//                 return res.status(200).json(
-//                     {
-//                         msg: 'User updated',
-//                         data: user
-//                     }
-//                 );
-//             }
-//         })
-//     } else {
-//         res.status(400).json({msg: `No user with the id of ${req.params.id}`});
-//     }
-// })
-
-// router.delete('/:id', (req, res) => {
-//     const found = users.some(user => user.id === parseInt(req.params.id));
-//     if(found) {
-//         res.json(
-//             {
-//                 msg: 'User deleted',
-//                 users: users.filter(user => user.id !== parseInt(req.params.id))});
-//     } else {
-//         res.status(400).json({msg: `No user with the id of ${req.params.id}`});
-//     }
-// });
-
-
-// module.exports = router;
+module.exports = router;
